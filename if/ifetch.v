8b75fc8c7306bc543cdb2271e1fba980830c3616
Fuga Kato <f_kato@puma.ddna.cs.tsukuba.ac.jp>
2017-10-27 16:08:22 +0900

    tested if, fix needed

--- if/ifetch.v
+++ if/ifetch.v
@@ -1,21 +1,19 @@
 module ifetch(clk, rst,
-          v_i, v_o,
+          v_o,
           inst_i, inst_o,
           branch_i, baddr_i, addr_o,
-          stall_i, stall_o);
+          stall_i);
 
 `include "include/params.vh"
 
     input clk, rst;
-    input v_i;
     output v_o;
     input [WORD - 1:0] inst_i;
     output [WORD - 1:0] inst_o;
     input branch_i; // whether branch or not
-    input baddr_i; // address to branch to
-    output addr_o; // address to read next time
+    input [ADDR - 1:0] baddr_i; // address to branch to
+    output [ADDR - 1:0] addr_o; // address to read next time
     input stall_i;
-    output stall_o;
 
     // pipeline registers
     reg v_r;
@@ -27,9 +25,6 @@ module ifetch(clk, rst,
     assign inst_o = inst_r;
     assign addr_o = addr_r;
 
-    // stall to previous stage
-    assign stall_o = (v_r & stall_i);
-
     always @(posedge clk or negedge rst) begin
         if (~rst) begin
             v_r <= 0;
@@ -38,13 +33,14 @@ module ifetch(clk, rst,
         end
         else begin
             if (~stall_i) begin
-                v_r <= v_i;
                 inst_r <= inst_i;
-                if (branch_i) begin
+                if (branch_i == 1'b1) begin
                     addr_r <= baddr_i;
+                    v_r <= 1'b0;
                 end
                 else begin
                     addr_r <= addr_r + 1;
+                    v_r <= 1'b1;
                 end
             end
         end
--- test/test_if.v
+++ test/test_if.v
@@ -1,10 +1,15 @@
 `timescale 1ns/100ps
 module test_if();
+`include "include/params.vh"
     parameter STEP = 10;
 
     reg clk, rst;
+    reg branch_r;
+    reg [ADDR - 1:0] baddr_r;
 
-    top top(.clk(clk), .rst(rst));
+    top top(.clk(clk), .rst(rst),
+         .v_i(1'b1), .stall_i(1'b0),
+         .branch_i(branch_r), .baddr_i(baddr_r));
 
     always begin
         #(STEP/2) clk = ~clk;
@@ -13,18 +18,31 @@ module test_if();
     initial begin
         clk = 1'b0;
         rst = 1'b0;
+        branch_r = 1'b0;
+        baddr_r = 0;
 
         #1.0;
         #(STEP * 128);
         rst = 1'b1;
         #(STEP);
 
-        #(STEP * 32);
+        #(STEP * 4);
+        branch_r = 1'b1;
+        baddr_r = 2;
+        #(STEP * 4);
         $finish;
     end // initial
 
     always @(posedge clk) begin
-        $display("inst_o: %h", top.ifetch1.inst_o);
+        $display("###### cycle ######");
+        $display("branch_i baddr_i addr_o   inst_i   inst_o v_o");
+        $display("       %b    %h   %h %h %h   %b",
+                top.ifetch1.branch_i,
+                top.ifetch1.baddr_i,
+                top.ifetch1.addr_o,
+                top.ifetch1.inst_i,
+                top.ifetch1.inst_o,
+                top.ifetch1.v_o);
     end // always @(posedge clk)
 endmodule
 
